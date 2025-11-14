499316606a9e34fcb4e9454aef1e6eff2b55ac64
Lucas Ferreira <65078868+LucasFrra@users.noreply.github.com>
2025-11-14 10:27:50 +0100

    add cv

---
+++ pages/cv.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center text-xl">
+    Redirection…
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  window.location.href = '/cv.pdf'
+})
+</script>
